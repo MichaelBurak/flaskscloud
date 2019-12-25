6273d9f2909f65485de5960187f2a56861f0ccfd
MichaelBurak <michaelrburak@gmail.com>
2019-12-25 10:13:36 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, send_file, make_response, Response
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import stylecloud
+from PIL import Image
+from starlette.responses import FileResponse
+import io
+
+import os 
+
+app = Flask(__name__,static_url_path='/static')
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+
+OUTPUT_NAME = "./static/out.png"
+
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+class MyForm(FlaskForm):
+    txt = StringField('Text here')
+    submit = SubmitField('Submit')
+
+
+def sc(tx):
+
+    stylecloud.gen_stylecloud(text=tx,
+                              colors=['#ecf0f1', '#3498db', '#e74c3c'],
+                              background_color='#1A1A1A', output_name=OUTPUT_NAME)
+    pil_im = Image.open(OUTPUT_NAME)
+    file_object = io.BytesIO()
+
+    # write PNG in file-object
+    pil_im.save(file_object, 'PNG')
+
+    # move to beginning of file so `send_file()` it will read from start
+    file_object.seek(0)
+
+    return send_file(file_object, mimetype='image/PNG')
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = MyForm()
+    img = []
+    
+    place = ""
+    if form.validate_on_submit():
+        img = sc(form.txt.data)
+        resp = make_response(render_template('img.html')) #here you could use make_response(render_template(...)) too
+        resp.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+        resp.headers['Cache-Control'] = 'public, max-age=0'
+        return resp
+    return render_template('index.html', img=img, form=form, place=place)
+
+
+@app.route('/img')
+def img():
+    return render_template('img.html', img=img)
+
+
+if __name__ == '__main__':
+    app.run()
+    #debug=true
